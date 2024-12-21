8073aa4b489026439c6687683b787fbdf535c3a2
KostyantynDudar <ecomaner@gmail.com>
2024-12-22 01:56:38 +0200

    Initial commit: - Added service configuration for oranta_django - Implemented reload script for complete project restart - Cleaned up unnecessary service files and legacy configurations - Configured Gunicorn service for production with 3 workers and threading

--- oranta_project/apps/main/views.py
+++ oranta_project/apps/main/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from apps.main.models import Article
 
 def home_view(request):
-    return render(request, "home.html")
+    return render(request, "main/home.html")
+
+def contact_view(request):
+    return render(request, "main/contacts.html")
+
+def article_list_view(request):
+    articles = Article.objects.all().order_by("-created_at")
+    return render(request, "main/articles.html", {"articles": articles})
+
+def article_detail_view(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, "main/article_detail.html", {"article": article})
