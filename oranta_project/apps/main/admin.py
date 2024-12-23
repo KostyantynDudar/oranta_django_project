528375ba9953f745d06bc38470422dbee907d477
KostyantynDudar <ecomaner@gmail.com>
2024-12-24 00:33:13 +0200

    shor list of parameter input client in main page

--- oranta_project/apps/main/admin.py
+++ oranta_project/apps/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import InsuranceApplication
 
-# Register your models here.
+@admin.register(InsuranceApplication)
+class InsuranceApplicationAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'tax_code', 'registration_address', 'deductible', 'created_at')
+    search_fields = ('phone_number', 'tax_code', 'registration_address')
+    list_filter = ('deductible', 'created_at')
