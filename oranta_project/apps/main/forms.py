088c5d9c3d00e58a763a373eaf66522ba64f52c8
KostyantynDudar <ecomaner@gmail.com>
2024-12-25 23:48:42 +0200

    add validatir phone_number format

--- oranta_project/apps/main/forms.py
+++ oranta_project/apps/main/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import InsuranceApplication
+import re
 
 class MultipleFileInput(forms.ClearableFileInput):
     allow_multiple_selected = True
@@ -15,9 +17,19 @@ class MultipleFileField(forms.FileField):
             return [single_file_clean(d, initial) for d in data]
         return single_file_clean(data, initial)
 
+def validate_phone_number(value):
+    if not re.match(r"^\+380\d{9}$", value):
+        raise ValidationError("Номер телефона должен быть в формате +380XXXXXXXXX.")
+
 class InsuranceApplicationForm(forms.ModelForm):
     tech_passports = MultipleFileField(label='Техпаспорта', required=False)
     personal_docs = MultipleFileField(label='Личные документы', required=False)
+    phone_number = forms.CharField(
+        label="Телефон",
+        max_length=13,
+        validators=[validate_phone_number],
+        help_text="Введите номер в формате +380XXXXXXXXX."
+    )
 
     class Meta:
         model = InsuranceApplication
